36f1e6bef49b18683df0f29dbf11ea109b682412
Jeff Yates <jeff@somewhatabstract.com>
2020-02-13 19:11:34 -0600

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+    // Automatically restore mocks between every test
+    restoreMocks: true,
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: "coverage",
+
+    // The test environment that will be used for testing
+    testEnvironment: "node",
+
+    // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
+    timers: "fake",
+
+    setupFilesAfterEnv: ["jest-extended"],
+};
