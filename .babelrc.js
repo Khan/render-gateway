973b5742e334dcdd884078dc5f76f049c35f84e2
Jeff Yates <jeff@somewhatabstract.com>
2020-02-13 18:48:21 -0600

    Add babel

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-commonjs */
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    node: "8",
+                },
+            },
+        ],
+        "@babel/preset-flow",
+    ],
+    plugins: ["@babel/plugin-proposal-class-properties"],
+    sourceMaps: true,
+};
