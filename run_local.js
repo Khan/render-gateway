78ab464f97b5ea0bab65824c96b7c7eb9abf2827
Jeff Yates <jeff@somewhatabstract.com>
2020-02-14 16:29:16 -0600

    Add basic local server
    TODO: Make it so that we can watch files and keep restarting it

---
+++ run_local.js
@@ -0,0 +1,15 @@
+// @noflow
+/**
+ * This is a simple local server for testing this code works.
+ */
+// eslint-disable-next-line import/no-commonjs
+const {default: runServer} = require("./dist/index.js");
+
+async function main() {
+    runServer({name: "DEV_LOCAL", port: 8080});
+}
+
+main().catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(`Error caught from main setup: ${err}`);
+});
