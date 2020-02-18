d33fb22a6ad9b4d488e5956c2e5ea5a9cd6238c4
Jeff Yates <jeff@somewhatabstract.com>
2020-02-17 21:19:21 -0600

    Add tests for pseudo-package exports

---
+++ src/shared/__tests__/index.test.js
@@ -0,0 +1,15 @@
+// @flow
+describe("index.js", () => {
+    it("should export what we expect it to export", async () => {
+        // Arrange
+        const importedModule = import("../index.js");
+
+        // Act
+        const result = await importedModule;
+
+        // Assert
+        expect(Object.keys(result).sort()).toEqual(
+            ["createLogger", "getRuntimeMode", "startGateway"].sort(),
+        );
+    });
+});
