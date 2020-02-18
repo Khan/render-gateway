ec24692b9d4c52e456b39a8249ebc1f51fbb0422
Jeff Yates <jeff@somewhatabstract.com>
2020-02-17 20:05:51 -0600

    Added log creation

---
+++ src/shared/__tests__/create-logger.test.js
@@ -0,0 +1,77 @@
+// @flow
+import winston from "winston";
+import {createLogger} from "../create-logger.js";
+
+/**
+ * The full LoggingWinston type will cause tests to hang weirdly, so we mock
+ * that and return a pretend one.
+ */
+jest.mock("@google-cloud/logging-winston", () => ({
+    LoggingWinston: function() {
+        return {
+            __FAKE_LOGGING_WINSTON__: true,
+            log: jest.fn(),
+            on: jest.fn(),
+        };
+    },
+}));
+
+describe("#createLogger", () => {
+    describe("during test", () => {
+        it("should write to a stream", () => {
+            // Arrange
+            const mockCreateLogger = jest.spyOn(winston, "createLogger");
+
+            // Act
+            createLogger("test", "silly");
+            const {transports} = mockCreateLogger.mock.calls[0][0];
+
+            // Assert
+            expect(transports).toBeInstanceOf(winston.transports.Stream);
+        });
+    });
+
+    describe("during development", () => {
+        it("should write to the console", () => {
+            // Arrange
+            const mockCreateLogger = jest.spyOn(winston, "createLogger");
+
+            // Act
+            createLogger("development", "silly");
+            const {transports} = mockCreateLogger.mock.calls[0][0];
+
+            // Assert
+            expect(transports).toBeInstanceOf(winston.transports.Console);
+        });
+    });
+
+    describe("during production", () => {
+        it("should write to stackdriver", () => {
+            // Arrange
+            const mockCreateLogger = jest.spyOn(winston, "createLogger");
+
+            // Act
+            createLogger("production", "silly");
+            const {transports} = mockCreateLogger.mock.calls[0][0];
+
+            // Assert
+            expect(transports).toHaveProperty("__FAKE_LOGGING_WINSTON__");
+        });
+    });
+
+    it("should log creation parameters to created logger", () => {
+        // Arrange
+        const debugSpy = jest.fn();
+        jest.spyOn(winston, "createLogger").mockReturnValue({
+            debug: debugSpy,
+        });
+
+        // Act
+        createLogger("test", "info");
+
+        // Assert
+        expect(debugSpy).toHaveBeenCalledWith(
+            "Created logger (Level=info Mode=test)",
+        );
+    });
+});
--- src/shared/types.js
+++ src/shared/types.js
@@ -1,4 +1,13 @@
 // @flow
+import type {
+    NpmLogLevels,
+    Logger as WinstonLogger,
+    Info as WinstonInfo,
+} from "winston";
+
+export type Info = WinstonInfo<NpmLogLevels>;
+export type LogLevel = $Keys<NpmLogLevels>;
+export type Logger = WinstonLogger<NpmLogLevels>;
 export type Runtime = "production" | "test" | "development";
 export type GatewayOptions = {
     name: string,
