1799bb49aa0de1614580a6abe581b09304626cdd
Jeff Yates <jeff@somewhatabstract.com>
2020-02-19 18:24:08 -0600

    Add test for the public API

--- src/ka-shared/start-trace-agent.js
+++ src/ka-shared/start-trace-agent.js
@@ -1,4 +1,8 @@
 // @flow
+/**
+ * This is the public facing, export for starting trace agent. Allows us to
+ * easily test the function itself separately.
+ */
 import {startTraceAgent} from "./start-trace-agent-impl.js";
 
 startTraceAgent();
