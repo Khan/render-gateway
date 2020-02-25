3d401690f58987c8c7b51be3c6fe1e5ad1aa9508
somewhatabstract <somewhatabstract@users.noreply.github.com>
2020-02-25 22:25:23 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/45163352)

--- dist/ka-shared/index.js
+++ dist/ka-shared/index.js
@@ -9,6 +9,14 @@ Object.defineProperty(exports, "getLogger", {
     return _getLogger.getLogger;
   }
 });
+Object.defineProperty(exports, "makeCommonServiceRouter", {
+  enumerable: true,
+  get: function () {
+    return _makeCommonServiceRouter.makeCommonServiceRouter;
+  }
+});
 
 var _getLogger = require("./get-logger.js");
+
+var _makeCommonServiceRouter = require("./routes/make-common-service-router.js");
 //# sourceMappingURL=index.js.map
