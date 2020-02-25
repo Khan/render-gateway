0e3550c8eb41d3423124ca5cd6420c8bae85fb4d
somewhatabstract <somewhatabstract@users.noreply.github.com>
2020-02-25 17:32:09 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/45031877)

---
+++ dist/shared/make-error-middleware.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.makeErrorMiddleware = void 0;
+
+var _expressWinston = _interopRequireDefault(require("express-winston"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Create middleware for reporting errors.
+ */
+const makeErrorMiddleware = logger =>
+/**
+ * Express-winston types aren't parameterized, so we suppress the error.
+ * $FlowIgnore
+ */
+_expressWinston.default.errorLogger({
+  level: "error",
+  winstonInstance: logger
+});
+
+exports.makeErrorMiddleware = makeErrorMiddleware;
+//# sourceMappingURL=make-error-middleware.js.map
