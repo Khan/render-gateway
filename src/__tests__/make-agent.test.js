272958acf50e0951f91589014b55614fba921121
Jeff Yates <jeff@somewhatabstract.com>
2020-03-06 16:50:24 -0600

    Add agent creation

---
+++ src/__tests__/make-agent.test.js
@@ -0,0 +1,44 @@
+// @flow
+import * as AgentKeepAlive from "agentkeepalive";
+import {makeAgent} from "../make-agent.js";
+
+import type {KeepAliveOptions} from "../types.js";
+
+jest.mock("agentkeepalive");
+
+describe("#makeAgent", () => {
+    it("should pass keep alive options to agentkeepalive", () => {
+        // Arrange
+        const options: KeepAliveOptions = {
+            workingSocketTimeout: 42,
+            freeSocketTimeout: 84,
+            maxFreeSockets: 255,
+            maxSockets: 128,
+        };
+        const agentKeepAliveSpy = jest.spyOn(AgentKeepAlive, "default");
+
+        // Act
+        makeAgent(options);
+
+        // Assert
+        expect(agentKeepAliveSpy).toHaveBeenCalledWith({
+            timeout: 42,
+            freeSocketTimeout: 84,
+            keepAlive: true,
+            maxFreeSockets: 255,
+            maxSockets: 128,
+        });
+    });
+
+    it("should return agentkeepalive", () => {
+        // Arrange
+        const fakeAgent = {};
+        jest.spyOn(AgentKeepAlive, "default").mockReturnValue(fakeAgent);
+
+        // Act
+        const result = makeAgent();
+
+        // Assert
+        expect(result).toBe(fakeAgent);
+    });
+});
