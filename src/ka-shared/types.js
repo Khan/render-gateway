9fbb5ffccb327bb4ad3429e0a8a464c39b3951d5
Jeff Yates <jeff@somewhatabstract.com>
2020-03-09 14:11:32 -0500

    Add KA Shared types commentary

--- src/ka-shared/types.js
+++ src/ka-shared/types.js
@@ -1,10 +1,38 @@
 // @flow
+/**
+ * A secret that is a string.
+ *
+ * This opaque type makes it clearer when secrets are being used and enforces
+ * the need for explicit casting if they must be used as a string.
+ */
 export opaque type SecretString = string;
+
+/**
+ * A named secret and its value.
+ */
 export type Secret = {
+    /**
+     * The name of the secret.
+     */
     name: string,
+
+    /**
+     * The secret. 🤫ssshhhhh!
+     */
     value: SecretString,
 };
+
+/**
+ * A collection of secrets keyed by their names.
+ */
 export type Secrets = {[string]: SecretString, ...};
+
+/**
+ * Configuration of secrets lookup.
+ *
+ * One variation provides a Google Cloud KMS configuration; the other provides
+ * means to map secrets dynamically.
+ */
 export type SecretsConfig =
     | {
           /**
