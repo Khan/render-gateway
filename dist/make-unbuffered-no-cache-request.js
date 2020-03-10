05cb4daa455af6c0a22c5cea05e38714be47b78d
Jeff Yates <jeff@khanacademy.org>
2020-03-10 16:04:44 -0500

    Implement makeUnbufferedNoCacheRequest (#47)
    * Add superagent and superagent-cache-plugin
    
    * Add agentkeepalive
    
    * Add request option types
    
    * Add agent creation
    
    * Implement isCacheable callback
    
    * Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/52634823)
    
    * Implement extractErrorString
    
    * Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/52655732)
    
    * Implement shouldRetry callback creation
    
    * Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/52658232)
    
    * Implement makeUnbufferedNoCacheRequest
    
    * Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/53099926)
    
    * Add tests
    
    Co-authored-by: somewhatabstract <somewhatabstract@users.noreply.github.com>

---
+++ dist/make-unbuffered-no-cache-request.js
@@ -0,0 +1,60 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.makeUnbufferedNoCacheRequest = void 0;
+
+var _superagent = _interopRequireDefault(require("superagent"));
+
+var _makeAgent = require("./make-agent.js");
+
+var _makeShouldRetry = require("./make-should-retry.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Make a request for a given URL without buffering or caching.
+ *
+ * This is not intended for direct use. Use makeRequest.
+ *
+ * @param {RenderGatewayOptions} options The options used to start the gateway.
+ * @param {string} url The URL to be requested.
+ * @param {Logger} logger The logger to use.
+ * @returns {SuperAgentRequest} A superagent request for the URL.
+ */
+const makeUnbufferedNoCacheRequest = (options, url, logger) => {
+  const {
+    name: gatewayName,
+    requests: requestOptions
+  } = options; // Get an agent.
+
+  const agent = (0, _makeAgent.makeAgent)(requestOptions === null || requestOptions === void 0 ? void 0 : requestOptions.keepAlive); // Build our main fetcher using the configured agent.
+
+  return _superagent.default.agent(agent).get(url)
+  /**
+   * Configure retries since superagent can handle this for us.
+   * We give it a callback so we can log the retry and, if we so choose
+   * in the future, decide whether we should allow any more. This would
+   * allow us to short circuit the retry count (the max retries still
+   * takes precedence over our callback response, so we can't retry
+   * forever).
+   */
+  .retry((requestOptions === null || requestOptions === void 0 ? void 0 : requestOptions.retries) || 2, (0, _makeShouldRetry.makeShouldRetry)(logger, requestOptions === null || requestOptions === void 0 ? void 0 : requestOptions.shouldRetry))
+  /**
+   * We add a user agent header so that we can easily identify our
+   * requests in logs.
+   *
+   * The header has a form like:
+   *     gateway-name (GAE_VERSION_STRING_HERE)
+   */
+  .set("User-Agent", `${gatewayName} (${process.env.GAE_VERSION || "UNKNOWN"})`)
+  /**
+   * Our default timeout is 1 minute, but we allow for it to be
+   * overridden by gateway options.
+   */
+  .timeout((requestOptions === null || requestOptions === void 0 ? void 0 : requestOptions.timeout) || 60000);
+};
+
+exports.makeUnbufferedNoCacheRequest = makeUnbufferedNoCacheRequest;
+//# sourceMappingURL=make-unbuffered-no-cache-request.js.map
---
+++ src/make-unbuffered-no-cache-request.js
@@ -0,0 +1,63 @@
+// @flow
+import superagent from "superagent";
+import type {SuperAgentRequest} from "superagent";
+import {makeAgent} from "./make-agent.js";
+import type {RenderGatewayOptions} from "./types.js";
+import {makeShouldRetry} from "./make-should-retry.js";
+import type {Logger} from "./shared/index.js";
+
+/**
+ * Make a request for a given URL without buffering or caching.
+ *
+ * This is not intended for direct use. Use makeRequest.
+ *
+ * @param {RenderGatewayOptions} options The options used to start the gateway.
+ * @param {string} url The URL to be requested.
+ * @param {Logger} logger The logger to use.
+ * @returns {SuperAgentRequest} A superagent request for the URL.
+ */
+export const makeUnbufferedNoCacheRequest = (
+    options: RenderGatewayOptions,
+    url: string,
+    logger: Logger,
+): SuperAgentRequest => {
+    const {name: gatewayName, requests: requestOptions} = options;
+
+    // Get an agent.
+    const agent = makeAgent(requestOptions?.keepAlive);
+
+    // Build our main fetcher using the configured agent.
+    return (
+        superagent
+            .agent(agent)
+            .get(url)
+            /**
+             * Configure retries since superagent can handle this for us.
+             * We give it a callback so we can log the retry and, if we so choose
+             * in the future, decide whether we should allow any more. This would
+             * allow us to short circuit the retry count (the max retries still
+             * takes precedence over our callback response, so we can't retry
+             * forever).
+             */
+            .retry(
+                requestOptions?.retries || 2,
+                makeShouldRetry(logger, requestOptions?.shouldRetry),
+            )
+            /**
+             * We add a user agent header so that we can easily identify our
+             * requests in logs.
+             *
+             * The header has a form like:
+             *     gateway-name (GAE_VERSION_STRING_HERE)
+             */
+            .set(
+                "User-Agent",
+                `${gatewayName} (${process.env.GAE_VERSION || "UNKNOWN"})`,
+            )
+            /**
+             * Our default timeout is 1 minute, but we allow for it to be
+             * overridden by gateway options.
+             */
+            .timeout(requestOptions?.timeout || 60000)
+    );
+};
