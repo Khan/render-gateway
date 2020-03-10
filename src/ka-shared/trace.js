668377d804698b7504694b957a7b6ede1f66573c
Jeff Yates <jeff@khanacademy.org>
2020-03-10 11:43:46 -0500

    Improve trace API a little (#42)
    * Improve trace API a little
    
    * Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/52628076)
    
    Co-authored-by: somewhatabstract <somewhatabstract@users.noreply.github.com>

--- src/ka-shared/trace.js
+++ src/ka-shared/trace.js
@@ -3,31 +3,63 @@ import * as traceAgent from "@google-cloud/trace-agent";
 import type {$Request} from "express";
 import {trace as traceImpl} from "../shared/index.js";
 import {getLogger} from "./get-logger.js";
+import type {Logger} from "../shared/index.js";
 
 import type {RequestWithLog, ITraceSession} from "../shared/types.js";
 
-/**
- * Start tracing an event.
- *
- * This will log the start of a trace and open a trace session, which is
- * returned. Use the returned session to end the trace when the traced event is
- * over. The traced event will be logged and also written to the Google Cloud
- * StackDriver Trace agent.
- *
- * Note that if startTraceAgent was never called, this will still log but the
- * StackDriver trace span creation will not actually happen.
- *
- * @param {string} name The name of the event being traced.
- * @param {TReq: RequestWithLog<$Request>} [request] The request being
- * fulfilled. This is used to determine if a request-scoped logger can be used.
- * @returns {ITraceSession} The new trace session that was created and is to be
- * used to end the session.
- */
-export const trace = <TReq: RequestWithLog<$Request>>(
+interface ITrace {
+    /**
+     * Start tracing an event.
+     *
+     * This will log the start of a trace and open a trace session, which is
+     * returned. Use the returned session to end the trace when the traced event is
+     * over. The traced event will be logged and also written to the Google Cloud
+     * StackDriver Trace agent.
+     *
+     * Note that if startTraceAgent was never called, this will still log but the
+     * StackDriver trace span creation will not actually happen.
+     *
+     * @param {string} name The name of the event being traced.
+     * @param {TReq: RequestWithLog<$Request>} [request] The request being
+     * fulfilled. This is used to determine if a request-scoped logger can be used.
+     * @returns {ITraceSession} The new trace session that was created and is to be
+     * used to end the session.
+     */
+    <TReq: RequestWithLog<$Request>>(
+        name: string,
+        request?: TReq,
+    ): ITraceSession;
+
+    /**
+     * Start tracing an event.
+     *
+     * This will log the start of a trace and open a trace session, which is
+     * returned. Use the returned session to end the trace when the traced event is
+     * over. The traced event will be logged and also written to the Google Cloud
+     * StackDriver Trace agent.
+     *
+     * Note that if startTraceAgent was never called, this will still log but the
+     * StackDriver trace span creation will not actually happen.
+     *
+     * @param {string} name The name of the event being traced.
+     * @param {Logger} logger The logger to be used for the trace.
+     * @returns {ITraceSession} The new trace session that was created and is to be
+     * used to end the session.
+     */
+    (name: string, logger: Logger): ITraceSession;
+}
+
+export const trace: ITrace = (
     name: string,
-    request?: TReq,
+    requestOrLogger: any,
 ): ITraceSession => {
-    const logger = getLogger(request);
     const tracer = traceAgent.get();
-    return traceImpl(logger, name, tracer);
+    if (
+        requestOrLogger == null ||
+        Object.prototype.hasOwnProperty.call(requestOrLogger, "url")
+    ) {
+        const logger = getLogger(requestOrLogger);
+        return traceImpl(logger, name, tracer);
+    }
+    return traceImpl(requestOrLogger, name, tracer);
 };
