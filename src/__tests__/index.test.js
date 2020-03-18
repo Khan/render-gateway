04f3b1305e17bc3f5c4e28a10da703856bb7800c
Jeff Yates <jeff@somewhatabstract.com>
2020-03-18 13:30:09 -0500

    9:Remove commented test code

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -10,15 +10,4 @@ describe("index.js", () => {
         // Assert
         expect(result).toContainAllKeys(["runServer"]);
     });
-
-    // it("should export status codes we expect", async () => {
-    //     // Arrange
-    //     const importedModule = await import("../index.js");
-
-    //     // Act
-    //     const result = importedModule.StatusCodes;
-
-    //     // Assert
-    //     expect(result).toContainAllKeys(["ok", "redirect", "notFound"]);
-    // });
 });
