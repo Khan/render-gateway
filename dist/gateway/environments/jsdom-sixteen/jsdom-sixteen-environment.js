75cba72344dba03441157d90faefe372fa34c318
somewhatabstract <somewhatabstract@users.noreply.github.com>
2020-06-04 16:38:16 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/125046320)

--- dist/gateway/environments/jsdom-sixteen/jsdom-sixteen-environment.js
+++ dist/gateway/environments/jsdom-sixteen/jsdom-sixteen-environment.js
@@ -248,7 +248,7 @@ class JSDOMSixteenEnvironment {
             (_closeables$i = closeables[i]) === null || _closeables$i === void 0 ? void 0 : (_closeables$i$close = _closeables$i.close) === null || _closeables$i$close === void 0 ? void 0 : _closeables$i$close.call(_closeables$i);
           } catch (e) {
             const simplifiedError = (0, _index.extractError)(e);
-            logger.error(`Closeable encountered an error during resource loader close: ${simplifiedError.error}`, _objectSpread({}, simplifiedError));
+            logger.error(`Closeable encountered an error during resource loader close: ${simplifiedError.error || ""}`, _objectSpread({}, simplifiedError));
           }
         }
         /**
