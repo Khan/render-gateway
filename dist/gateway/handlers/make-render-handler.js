6e807e4156a19e6c7eaf04611b399003ca7c1f19
jeresig <jeresig@users.noreply.github.com>
2020-06-25 20:59:24 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/147922065)

--- dist/gateway/handlers/make-render-handler.js
+++ dist/gateway/handlers/make-render-handler.js
@@ -9,6 +9,12 @@ var _index = require("../../ka-shared/index.js");
 
 var _handleError = require("./handle-error.js");
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 /**
  * Handle a request as a render.
  *
@@ -52,7 +58,8 @@ async function renderHandler(renderEnvironment, errorHandler, defaultErrorRespon
     const renderAPI = {
       trace: traceFn,
       logger,
-      headers: req.headers
+      // Passthrough the request headers to the render environment
+      headers: _objectSpread({}, req.headers)
     };
     /**
      * Defer this bit to the render callback.
