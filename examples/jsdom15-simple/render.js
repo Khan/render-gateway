ab680c417b365493025f8c2cb3b1a49de75c633d
Jeff Yates <jeff@khanacademy.org>
2020-06-30 22:35:23 -0500

    Add jsdom15 example

---
+++ examples/jsdom15-simple/render.js
@@ -0,0 +1,9 @@
+// @flow
+window["__jsdom_env_register"](() => {
+    // This is where we can return our result.
+    return Promise.resolve({
+        body: `You asked us to render ${window._API.url} with JSDOM 15`,
+        status: 200,
+        headers: {},
+    });
+});
--- examples/jsdom16-simple/render.js
+++ examples/jsdom16-simple/render.js
@@ -2,7 +2,7 @@
 window["__jsdom_env_register"](() => {
     // This is where we can return our result.
     return Promise.resolve({
-        body: `You asked us to render ${window._API.url}`,
+        body: `You asked us to render ${window._API.url} with JSDOM 16`,
         status: 200,
         headers: {},
     });
