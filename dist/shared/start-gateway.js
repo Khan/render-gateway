da4eb92534860bc35eb878cb32c8b139b70a081b
Jeff Yates <jeff@khanacademy.org>
2020-07-30 17:19:23 -0500

    Update build artifacts for manual testing

--- dist/shared/start-gateway.js
+++ dist/shared/start-gateway.js
@@ -98,6 +98,28 @@ async function startGateway(options, app) {
     logger.info(`${name} running at http://${host}:${port}`);
   });
   /**
+   * We use keep-alive connections with other resources. These can prevent
+   * the gateway process from shutting down if they are open when we're
+   * trying to close. So, let's track them and provide a means for us to\
+   * destroy them.
+   */
+
+  const connections = {};
+
+  const closeConnections = () => {
+    for (const connection of Object.values(connections)) {
+      connection.destroy();
+    }
+  };
+
+  gateway === null || gateway === void 0 ? void 0 : gateway.on("connection", connection => {
+    const key = `${connection.remoteAddress}:${connection.remotePort}`;
+    connections[key] = connection;
+    connection.on("close", () => {
+      delete connections[key];
+    });
+  });
+  /**
    * In this server is being run using a process manager, such as PM2, we
    * may be asked to shutdown gracefully. We do this be listening for the
    * SIGINT signal and then close the server. This prevents new connections
@@ -123,6 +145,7 @@ async function startGateway(options, app) {
           process.exit(0);
         }
       });
+      closeConnections();
     } catch (err) {
       logger.error(`Error closing gateway: ${err && err.message || "Unknown Error"}`);
       process.exit(1);
