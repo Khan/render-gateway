480e6feb15fc922f1aa539e5b9378364613c0a02
latteier <latteier@users.noreply.github.com>
2021-03-08 21:04:09 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/633633259)

--- dist/gateway/middleware/log-request-info-middleware.js
+++ dist/gateway/middleware/log-request-info-middleware.js
@@ -9,9 +9,19 @@ var _index = require("../../shared/index.js");
 
 /**
  * Simple middleware that logs some info about the incoming request.
+ *
+ * We log both at the info and debug levels. The info log line doesn't
+ * include the headers since they give us problems with exporting our
+ * info logs to bigquery. The issue is that we end up with a bigquery
+ * column per unique header name, and so we run out of columns.
  */
 function logRequestInfoMiddleware(req, res, next) {
-  (0, _index.getLogger)(req).info(`Request received: ${req.url}`, {
+  const logger = (0, _index.getLogger)(req);
+  logger.info(`Request received: ${req.url}`, {
+    method: req.method,
+    url: req.url
+  });
+  logger.debug(`Request received: ${req.url}`, {
     headers: req.headers,
     method: req.method,
     url: req.url
