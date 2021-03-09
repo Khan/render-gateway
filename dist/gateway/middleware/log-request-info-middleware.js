565ad59021ee0b29ae8cab97e7fe923f567f11cf
latteier <latteier@users.noreply.github.com>
2021-03-09 04:37:37 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/634611067)

--- dist/gateway/middleware/log-request-info-middleware.js
+++ dist/gateway/middleware/log-request-info-middleware.js
@@ -10,19 +10,15 @@ var _index = require("../../shared/index.js");
 /**
  * Simple middleware that logs some info about the incoming request.
  *
- * We log both at the info and debug levels. The info log line doesn't
- * include the headers since they give us problems with exporting our
- * info logs to bigquery. The issue is that we end up with a bigquery
- * column per unique header name, and so we run out of columns.
+ * We flatten the headers since they give us problems with exporting
+ * our info logs to bigquery if we don't. The issue is that we end up
+ * with a bigquery column per unique header name, and so we run out of
+ * columns.
  */
 function logRequestInfoMiddleware(req, res, next) {
-  const logger = (0, _index.getLogger)(req);
-  logger.info(`Request received: ${req.url}`, {
-    method: req.method,
-    url: req.url
-  });
-  logger.debug(`Request received: ${req.url}`, {
-    headers: req.headers,
+  const flattenedHeaders = Object.keys(req.headers).reduce((headers, key) => headers + `${key}: ${req.headers[key]}\n`, "");
+  (0, _index.getLogger)(req).info(`Request received: ${req.url}`, {
+    headers: flattenedHeaders,
     method: req.method,
     url: req.url
   });
