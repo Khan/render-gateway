1b49000770f64d876224b145e3f4a075352fd684
Jeff Yates <jeff@khanacademy.org>
2021-04-15 15:24:41 -0500

    Update workflow and force new dist (#476)
    ## Summary:
    This updates our workflow action versions and makes some code changes to invoke
    a new build that will need to push changes to the repo, thus testing the secrets
    that I have updated.
    
    Bonus: more test coverage.
    
    Issue: None
    
    ## Test plan:
    Creating this PR should execute the new workflow
    
    Author: somewhatabstract
    
    Reviewers: somewhatabstract, jeresig
    
    Required Reviewers: 
    
    Approved by: jeresig
    
    Checks: ✅ codecov/project, ✅ Test (macOS-latest, 14.x), ✅ Test (macOS-latest, 12.x), ✅ Test (ubuntu-latest, 14.x), ✅ Coverage (ubuntu-latest, 12.x), ✅ Publish to branch, ✅ Lint and flow check (ubuntu-latest, 12.x), ✅ Prime node_modules cache (macOS-latest, 14.x), ✅ Prime node_modules cache (macOS-latest, 12.x), ✅ Prime node_modules cache (ubuntu-latest, 14.x), ✅ Prime node_modules cache (ubuntu-latest, 12.x), ⏭  Auto-approve dependabot PRs
    
    Pull request URL: https://github.com/Khan/render-gateway/pull/476

--- src/shared/ka-error.js
+++ src/shared/ka-error.js
@@ -12,17 +12,25 @@ export default class KAError<TKind: ErrorKind> extends Error {
     sourceError: ?SimplifiedError;
 
     /**
-     * Construct KAError instance.
+     * Construct a KAError instance.
      *
      * @param {string} message The error message.
-     * @param {TKind} kind The kind of KA error.
+     * @param {TKind} kind The kind of error.
      * @param {Error} [sourceError] The original error that spawned this one.
      */
     constructor(message: string, kind: TKind, sourceError: ?Error = null) {
         super(message);
+
+        // Set the name so we get a nice error output, like
+        // KAInternalError
         this.name = `KA${kind}Error`;
+
+        // The kind of error which could be used for categorization with
+        // other error sources that use the same error taxonomy.
         this.kind = kind;
 
+        // If there is a source error that we're wrapping, we also want to
+        // unpack that and attach it for additional diagnostics.
         if (sourceError != null) {
             this.sourceError = extractError(sourceError);
         }
