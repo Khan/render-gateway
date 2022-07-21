b50239171bdc2fe0caf5c0ec37db577f45155de7
somewhatabstract <somewhatabstract@users.noreply.github.com>
2022-07-21 21:07:21 +0000

    Updating branch with build artifacts (https://github.com/Khan/render-gateway/actions/runs/2714703516)

--- dist/gateway/environments/jsdom-sixteen/jsdom-sixteen-environment.js
+++ dist/gateway/environments/jsdom-sixteen/jsdom-sixteen-environment.js
@@ -36,7 +36,7 @@ class JSDOMSixteenEnvironment {
          * we can retrieve those files as well as support retrieving
          * additional files within our JSDOM environment.
          */
-        const fileURLs = await this._configuration.getFileList(url, renderAPI, (url, options) => resourceLoader.fetch(url, options));
+        const fileURLs = await this._configuration.getFileList(url, renderAPI, url => resourceLoader.fetch(url));
         traceSession.addLabel("fileCount", fileURLs.length);
         /**
          * Now let's use the resource loader to get the files.
